c1ad375fbc7d061788f5abd52de9248b3318fd45
Gabriel Abreu <GabrielAbreudeLima1@gmail.com>
2023-01-13 00:07:18 -0300

    first commit

---
+++ java/vava.js
@@ -0,0 +1,84 @@
+function dispararModal(){
+	let resgatar = document.getElementById('modal')
+
+	resgatar.style.visibility = "visible"
+	resgatar.style.position = "relative"
+	resgatar.style.top = "auto";
+	resgatar.style.opacity = '1';
+
+	
+}
+
+
+
+function fechar(){
+
+	let resgatar = document.getElementById('modal')
+	resgatar.style.visibility = "hidden"
+	resgatar.style.position = "absolute"
+	resgatar.style.top = "1000px";
+	resgatar.style.opacity = '0';
+
+
+	// let node = document.getElementById("modal");
+	// if (node.parentNode) {
+	//   node.parentNode.removeChild(node);
+	// }
+}
+
+
+
+function dispararModal2(){
+	let resgatar2 = document.getElementById('modal2')
+
+	resgatar2.style.visibility = "visible"
+	resgatar2.style.position = "relative"
+	resgatar2.style.top = "auto";
+	resgatar2.style.opacity = '1';
+
+}
+
+
+
+function fechar2(){
+
+	let resgatar2 = document.getElementById('modal2')
+	resgatar2.style.visibility = "hidden"
+	resgatar2.style.position = "absolute"
+	resgatar2.style.top = "-120px";
+	resgatar2.style.opacity = '1';
+
+	// let node = document.getElementById("modal");
+	// if (node.parentNode) {
+	//   node.parentNode.removeChild(node);
+	// }
+}
+
+
+
+function dispararModal3(){
+	let resgatar3 = document.getElementById('modal3')
+
+	resgatar3.style.visibility = "visible"
+	resgatar3.style.position = "relative"
+	resgatar3.style.top = "auto";
+	resgatar3.style.opacity = '1';
+
+}
+
+
+function fechar3(){
+
+	let resgatar3 = document.getElementById('modal3')
+	resgatar3.style.visibility = "hidden"
+	resgatar3.style.position = "absolute"
+	resgatar3.style.top = "1000px";
+	resgatar3.style.opacity = '0';
+
+	// let node = document.getElementById("modal");
+	// if (node.parentNode) {
+	//   node.parentNode.removeChild(node);
+	// }
+}
+
+

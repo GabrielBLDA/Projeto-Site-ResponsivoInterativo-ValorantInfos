587d104d485e5bf78124c42204aadbf20e0db0a3
Gabriel Abreu <GabrielAbreudeLima1@gmail.com>
2023-01-14 00:30:57 -0300

    layout v1.2

--- java/vava.js
+++ java/vava.js
@@ -1,84 +1,56 @@
-function dispararModal(){
-	let resgatar = document.getElementById('modal')
+function layoutArmas() {
+	let recuperar = document.getElementById('armas')
+	let testar = document.getElementById('agentes')
 
-	resgatar.style.visibility = "visible"
-	resgatar.style.position = "relative"
-	resgatar.style.top = "auto";
-	resgatar.style.opacity = '1';
-
-	
+	if (testar.style.display === "contents") {
+		testar.style.display = "none";
+		recuperar.style.display = "contents"
+	} else {
+		recuperar.style.display = "contents"
+	}	
 }
 
 
 
-function fechar(){
-
-	let resgatar = document.getElementById('modal')
-	resgatar.style.visibility = "hidden"
-	resgatar.style.position = "absolute"
-	resgatar.style.top = "1000px";
-	resgatar.style.opacity = '0';
+function layoutAgentes() {
+	let recuperar = document.getElementById('agentes')
+	let testar = document.getElementById('armas')
 
 
-	// let node = document.getElementById("modal");
-	// if (node.parentNode) {
-	//   node.parentNode.removeChild(node);
-	// }
-}
-
-
-
-function dispararModal2(){
-	let resgatar2 = document.getElementById('modal2')
-
-	resgatar2.style.visibility = "visible"
-	resgatar2.style.position = "relative"
-	resgatar2.style.top = "auto";
-	resgatar2.style.opacity = '1';
+	if (testar.style.display === "contents") {
+		testar.style.display = "none";
+		recuperar.style.display = "contents"
+	} else {
+		recuperar.style.display = "contents"
+	}
 
 }
 
+//TESTE 1 FUNÇÃO
+// function layoutArmas() {
+// 	let recuperar = document.getElementById('armas')
+// 	let testar = document.getElementById('agentes')
 
-
-function fechar2(){
-
-	let resgatar2 = document.getElementById('modal2')
-	resgatar2.style.visibility = "hidden"
-	resgatar2.style.position = "absolute"
-	resgatar2.style.top = "-120px";
-	resgatar2.style.opacity = '1';
-
-	// let node = document.getElementById("modal");
-	// if (node.parentNode) {
-	//   node.parentNode.removeChild(node);
-	// }
-}
+// 	if (testar.style.display === "contents") {
+// 		testar.style.display = "none";
+// 		recuperar.style.display = "contents"
+// 	} else {
+// 		recuperar.style.display = "contents"
+// 	}	
+// }
 
 
 
-function dispararModal3(){
-	let resgatar3 = document.getElementById('modal3')
-
-	resgatar3.style.visibility = "visible"
-	resgatar3.style.position = "relative"
-	resgatar3.style.top = "auto";
-	resgatar3.style.opacity = '1';
-
-}
+// function layoutAgentes() {
+// 	let recuperar = document.getElementById('agentes')
+// 	let testar = document.getElementById('armas')
 
 
-function fechar3(){
-
-	let resgatar3 = document.getElementById('modal3')
-	resgatar3.style.visibility = "hidden"
-	resgatar3.style.position = "absolute"
-	resgatar3.style.top = "1000px";
-	resgatar3.style.opacity = '0';
-
-	// let node = document.getElementById("modal");
-	// if (node.parentNode) {
-	//   node.parentNode.removeChild(node);
-	// }
-}
+// 	if (testar.style.display === "contents") {
+// 		testar.style.display = "none";
+// 		recuperar.style.display = "contents"
+// 	} else {
+// 		recuperar.style.display = "contents"
+// 	} }
 
 

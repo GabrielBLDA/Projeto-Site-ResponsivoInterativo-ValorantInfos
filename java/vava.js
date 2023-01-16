eb250161785d2c9b450eeab97c9d1dd58a7f8a49
Gabriel Abreu <GabrielAbreudeLima1@gmail.com>
2023-01-15 23:11:52 -0300

    v:1.2 inserindo HUD de Agentes

--- java/vava.js
+++ java/vava.js
@@ -27,36 +27,70 @@ function layoutAgentes() {
 
 }
 
-
 function aviso(){
 	alert('Ainda não programei este botão. Tente selecionar Outro!!!.')
 }
 
-//TESTE 1 FUNÇÃO
-// function layoutArmas() {
-// 	let recuperar = document.getElementById('armas')
-// 	let testar = document.getElementById('agentes')
-
-// 	if (testar.style.display === "contents") {
-// 		testar.style.display = "none";
-// 		recuperar.style.display = "contents"
-// 	} else {
-// 		recuperar.style.display = "contents"
-// 	}	
-// }
 
 
 
-// function layoutAgentes() {
-// 	let recuperar = document.getElementById('agentes')
-// 	let testar = document.getElementById('armas')
+//função skills campeao
+
+function legendaSkill(value){
+	
+	if (value === 'q') {
+		let recuperar = document.getElementById('skillRazeQ')
+		recuperar.style.display = "contents"
+
+		let esconderC = document.getElementById('skillRazeC')
+		let esconderE = document.getElementById('skillRazeE')
+		let esconderX = document.getElementById('skillRazeX')
+
+		esconderC.style.display = "none"
+		esconderE.style.display = "none"
+		esconderX.style.display = "none"
+	}
+
+	if (value === 'c') {
+		let recuperar = document.getElementById('skillRazeC')
+		recuperar.style.display = "contents"
+
+		let esconderQ = document.getElementById('skillRazeQ')
+		let esconderE = document.getElementById('skillRazeE')
+		let esconderX = document.getElementById('skillRazeX')
+
+		esconderQ.style.display = "none"
+		esconderE.style.display = "none"
+		esconderX.style.display = "none"
+	} 
+
+	if (value === 'e') {
+		let recuperar = document.getElementById('skillRazeE')
+		recuperar.style.display = "contents"
+
+		let esconderQ = document.getElementById('skillRazeQ')
+		let esconderC = document.getElementById('skillRazeC')
+		let esconderX = document.getElementById('skillRazeX')
+
+		esconderQ.style.display = "none"
+		esconderC.style.display = "none"
+		esconderX.style.display = "none"
+
+	} 
+	if (value === 'x') {
+		let recuperar = document.getElementById('skillRazeX')
+		recuperar.style.display = "contents"
+
+		let esconderQ = document.getElementById('skillRazeQ')
+		let esconderE = document.getElementById('skillRazeE')
+		let esconderC = document.getElementById('skillRazeC')
+
+		esconderQ.style.display = "none"
+		esconderE.style.display = "none"
+		esconderC.style.display = "none"
+	}  	
+}
 
 
-// 	if (testar.style.display === "contents") {
-// 		testar.style.display = "none";
-// 		recuperar.style.display = "contents"
-// 	} else {
-// 		recuperar.style.display = "contents"
-// 	} }
 
 
